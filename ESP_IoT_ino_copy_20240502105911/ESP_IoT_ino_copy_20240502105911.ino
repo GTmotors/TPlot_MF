c0e92161e268ea5314f6c517452ad6288c0b566f
GTmotors <mathice.fve@gmail.com>
2024-05-02 15:56:40 +0200

    Update ESP_IoT_ino_copy_20240502105911.ino

--- ESP_IoT_ino_copy_20240502105911/ESP_IoT_ino_copy_20240502105911.ino
+++ ESP_IoT_ino_copy_20240502105911/ESP_IoT_ino_copy_20240502105911.ino
@@ -1,9 +1,31 @@
+#include <ESP8266WiFi.h>
+#include "secrets.h" // Inclure le fichier secrets.h
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(100);
 
+  // Connexion au réseau WiFi
+  WiFi.begin(ssid, password); // Utilisation des variables du fichier secrets.h
+
+  Serial.println("Connexion au réseau WiFi en cours...");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("Connexion WiFi établie !");
 }
-*test2*
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void loop() {
+  // Lecture de la qualité du signal WiFi (RSSI)
+  int32_t rssi = WiFi.RSSI();
+
+  Serial.print("Qualité du signal WiFi (RSSI) : ");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+
+  delay(10000); // Attente de 10 secondes entre chaque lecture
 }
